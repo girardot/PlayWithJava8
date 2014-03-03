01544b55485fde8f4dd217dc7c0e097d93b04cfa
ibeauvais <ivan.beauvais@gmail.com>
2014-03-03 13:04:40 +0100

    refacto java 8

--- src/main/java/fr/xebia/xke/step2/BasicCollectionOperations.java
+++ src/main/java/fr/xebia/xke/step2/BasicCollectionOperations.java
@@ -3,7 +3,6 @@ package fr.xebia.xke.step2;
 import fr.xebia.xke.fr.xebia.xke.java8.data.User;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,48 +12,26 @@ public class BasicCollectionOperations {
     public static void resetPassword(List<User> users) {
         //TODO :Refactor with new Iterable Method
 
-        for (User user : users) {
-            user.password = null;
-        }
+        users.forEach(user -> user.password = null);
     }
 
     public static void removeExpiredUsers(List<User> users) {
         //TODO :Refactor with new Collection Method
 
-        List<User> usersToRemove = new ArrayList<>();
-
-        for (User user : users) {
-            if (user.isExpired()) {
-                usersToRemove.add(user);
-            }
-        }
-
-        users.removeAll(usersToRemove);
+        users.removeIf(User::isExpired);
     }
 
     public static void addOneDayToDates(List<LocalDate> localDates) {
         //TODO :Refactor with new List Method
 
-        for (int i = 0; i < localDates.size(); i++) {
-            LocalDate localDate = localDates.get(i);
-            LocalDate newDate = localDate.plusDays(1);
-
-            localDates.set(i, newDate);
-        }
+        localDates.replaceAll(date -> date.plusDays(1));
     }
 
     public static Map<String, Integer> countWord(List<String> words) {
 
         Map<String, Integer> count = new HashMap<>();
 
-        for (String word : words) {
-            Integer countOfCurrentWord = count.get(word);
-            if (countOfCurrentWord == null) {
-                countOfCurrentWord = 0;
-            }
-
-            count.put(word, countOfCurrentWord + 1);
-        }
+        words.forEach(word -> count.merge(word, 1, (oldValue, newValue) -> oldValue + newValue));
 
         return count;
     }
