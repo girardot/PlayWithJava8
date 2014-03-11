d9dd3b758a78178b39db846a94900d7c74300dee
ibeauvais <ivan.beauvais@gmail.com>
2014-03-11 12:54:04 +0100

    add splitEvenAndOddNumber

--- src/main/java/fr/xebia/xke/java8/step4/NumberUtils.java
+++ src/main/java/fr/xebia/xke/java8/step4/NumberUtils.java
@@ -2,7 +2,7 @@ package fr.xebia.xke.java8.step4;
 
 import com.sun.istack.internal.Nullable;
 
-import java.util.Random;
+import java.util.*;
 
 public class NumberUtils {
 
@@ -16,6 +16,25 @@ public class NumberUtils {
         }
 
         return randomValues;
+    }
+
+    public static Map<Boolean, List<Integer>> splitEvenAndOddNumber(int[] numbers) {
+        List<Integer> evenNumber = new ArrayList<>();
+        List<Integer> oddNumber = new ArrayList<>();
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                evenNumber.add(number);
+            } else {
+                oddNumber.add(number);
+            }
+        }
+
+        Map<Boolean, List<Integer>> result = new HashMap<>();
+        result.put(Boolean.TRUE, evenNumber);
+        result.put(Boolean.FALSE, oddNumber);
+
+        return result;
     }
 
     private static Random getRandom(Long seed) {
