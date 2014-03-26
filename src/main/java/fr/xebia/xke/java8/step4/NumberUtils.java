3ca9e5095aaf27cfa9ffe974e2ba5bbcc41d969e
ibeauvais <ivan.beauvais@gmail.com>
2014-03-26 21:51:46 +0100

    add fibonacci

--- src/main/java/fr/xebia/xke/java8/step4/NumberUtils.java
+++ src/main/java/fr/xebia/xke/java8/step4/NumberUtils.java
@@ -6,6 +6,14 @@ import java.util.*;
 
 public class NumberUtils {
 
+    private static Map<Integer, Long> fibonacciValues = new HashMap<>();
+
+    static {
+        fibonacciValues.put(0, 0L);
+        fibonacciValues.put(1, 1L);
+        fibonacciValues.put(2, 1L);
+    }
+
     /**
      * Return the 'number' random int between 0 and 'number * 10'
      *
@@ -45,6 +53,22 @@ public class NumberUtils {
         result.put(Boolean.FALSE, oddNumber);
 
         return result;
+    }
+
+    public static List<Long> fibonacci(int expectedNumberResult) {
+        //TODO: replace for by stream generation with IntStream
+
+        List<Long> result = new ArrayList<>(expectedNumberResult);
+
+        for (int i = 1; i <= expectedNumberResult; i++) {
+
+            result.add(fibonacciComputation(i));
+        }
+        return result;
+    }
+
+    private static long fibonacciComputation(int number) {
+        return fibonacciValues.computeIfAbsent(number, newNumber -> fibonacciComputation(newNumber - 1) + fibonacciComputation(newNumber - 2));
     }
 
     private static Random getRandom(Long seed) {
